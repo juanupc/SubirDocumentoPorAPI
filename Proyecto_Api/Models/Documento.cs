b7cad785c3517cc8a331ac92193d8c7f049a04ea
juanupc <jandressalcedo@unicesar.edu.co>
2022-12-27 08:55:14 -0500

    API Subir documentos

---
+++ Proyecto_Api/Models/Documento.cs
@@ -0,0 +1,11 @@
+﻿namespace Proyecto_Api.Models
+{
+    public class Documento
+    {
+        public int IdDocumento { get; set; }
+        public string Descripcion { get; set; }
+        public string Ruta { get; set; }
+
+        public IFormFile Archivo { get; set; }
+    }
+}
